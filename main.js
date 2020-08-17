c7e118abee8b160ac4399ad37119d01be4b55774
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-08-17 13:48:28 -0700

    Done

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-function isHot() {
+function isHot(temp) {
+ 
+    return (temp >= 80) ? "Yes, it is indeed hot." : "No, it is not hot."
+    
+  }
 
+
+function helloThere(name) {
+
+    return (name.length < 6) ? `Hello, ${name}` : `Hi, ${name}`;
 }
 
 
-function helloThere() {
-
-}
-
-
-function goodbyeYou() {
+function goodbyeYou(person) {
+  return (person === undefined) ? "Goodbye, stranger" : `Goodbye, ${person}`;
 
 }
 
